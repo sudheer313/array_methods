07dbdc7379d1a581401b12ba236898aaf315f5ae
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-03-07 00:16:39 +1100

    updata map and filter using compplex data

--- index.js
+++ index.js
@@ -7,14 +7,81 @@
 // console.log(sum);
 // numbers.forEach((number) => console.log(number));
 
-const arr = [
-  { id: 1, name: "John" },
-  { id: 2, name: "Jane" },
-  { id: 3, name: "Bob" },
-];
+// const arr = [
+//   { id: 1, name: "John" },
+//   { id: 2, name: "Jane" },
+//   { id: 3, name: "Bob" },
+// ];
 
-arr.map((item) => {
-  console.log(item.id, item.name);
-});
+// arr.map((item) => {
+//   console.log(item.id, item.name);
+// });
 
-arr.filter((item) => console.log(item.name === "John"));
+// arr.filter((item) => console.log(item.name === "John"));
+
+const data = {
+  users: [
+    {
+      id: 1,
+      name: "John",
+      age: 35,
+      address: {
+        street: "123 Main St",
+        city: "New York",
+        state: "NY",
+        zip: "10001",
+      },
+      hobbies: ["reading", "swimming", "traveling"],
+      friends: [
+        { id: 2, name: "Mary", age: 30 },
+        { id: 3, name: "Tom", age: 40 },
+      ],
+    },
+    {
+      id: 2,
+      name: "Mary",
+      age: 30,
+      address: {
+        street: "456 Elm St",
+        city: "Los Angeles",
+        state: "CA",
+        zip: "90001",
+      },
+      hobbies: ["hiking", "biking", "skiing"],
+      friends: [
+        { id: 1, name: "John", age: 35 },
+        { id: 3, name: "Tom", age: 40 },
+      ],
+    },
+    {
+      id: 3,
+      name: "Tom",
+      age: 40,
+      address: {
+        street: "789 Oak St",
+        city: "Chicago",
+        state: "IL",
+        zip: "60601",
+      },
+      hobbies: ["painting", "cooking", "gardening"],
+      friends: [
+        { id: 1, name: "John", age: 35 },
+        { id: 2, name: "Mary", age: 30 },
+      ],
+    },
+  ],
+};
+
+// data.users.map((user) => {
+//   console.log(
+//     user.id,
+//     user.name,
+//     user.age,
+//     user.address,
+//     user.hobbies,
+//     user.friends
+//   );
+// });
+
+const usersover35 = data.users.filter((user) => user.age > 35);
+console.log(usersover35);
