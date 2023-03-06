5f34941723c72491021a4303677fad91f4818304
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-03-06 23:49:56 +1100

    update index.js with nodemon

---
+++ index.js
@@ -0,0 +1,5 @@
+const numbers = [1, 2, 3, 4, 5];
+const squares = numbers.map((number) => number * number);
+console.log(squares);
+const oddNumbers = numbers.filter((number) => number % 2 == 0);
+console.log(oddNumbers);
