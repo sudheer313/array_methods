aaf9fea96c0a6258614dc315f624e3547d751282
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-03-06 23:54:22 +1100

    update with array methods with a array

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const squares = numbers.map((number) => number * number);
 console.log(squares);
 const oddNumbers = numbers.filter((number) => number % 2 == 0);
 console.log(oddNumbers);
+const sum = numbers.reduce((acc, currentValue) => acc + currentValue);
+console.log(sum);
+numbers.forEach((number) => console.log(number));
